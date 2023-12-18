03457de25b83006533502b6424295783317ea001
Andrew Lapidus <drewtl1997@gmail.com>
2023-12-18 17:17:47 -0500

    Inital commit

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+
+    public void run(){
+        System.out.println("Hello World");
+    }
+}
